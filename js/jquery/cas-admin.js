74787856e1276d0ca80237d1be9498b6ab753d7e
inkmarts <yiyangh@compandsave.com>
2014-10-27 14:12:06 -0700

    order operation

--- js/jquery/cas-admin.js
+++ js/jquery/cas-admin.js
@@ -4,27 +4,28 @@ if (window.location.href.match("/sales_order/index/")) {
 	//create div
 	var admin_order_filter = document.createElement('div');
 	admin_order_filter.id = "admin_order_filter";
-	admin_order_filter.innerHTML = "<br><input type='button' value='Auto Ship' id='order_filter_autoship'><br>";
-	admin_order_filter.innerHTML += "<br><input type='button' value='Small Order' id='order_filter_small'><br>";
-	admin_order_filter.innerHTML += "<br><input type='button' value='Large Order' id='order_filter_large'><br>";
-	admin_order_filter.innerHTML += "<br><input type='button' value='Ready To Ship' id='order_filter_readytoship'><br>";
+	admin_order_filter.innerHTML = "<br><input type='button' class='form-button' value='Auto Ship' id='order_filter_autoship'><br>";
+	admin_order_filter.innerHTML += "<br><input type='button' class='form-button' value='Small Order' id='order_filter_small'><br>";
+	admin_order_filter.innerHTML += "<br><input type='button' class='form-button' value='Large Order' id='order_filter_large'><br>";
+	admin_order_filter.innerHTML += "<br><input type='button' class='form-button' value='Ready To Ship' id='order_filter_readytoship'><br>";
 //position,fixed top,300px; 
-	admin_order_filter.style = "display:none;width:100px; height:500px; background-color:white;";
+	admin_order_filter.style = "display:none;width:100px; height:500px; background-color:#669;position:absolute;";
 	var admin_filter_block  = document.createElement('div');
 	var admin_filter_header = document.createElement('div');
-	admin_filter_header.innerHTML = "<p>CAS Filter</p>";
-	admin_filter_header.style = "height:30px;width:100px;background-color:white;";
+	admin_filter_header.innerHTML = "<input type='button' class='form-button' value='Order Filter'>";
+	admin_filter_header.style = "margin-top:10px;";
 
 	admin_filter_block.id = "admin_filter_block";
-	admin_filter_block.style = "position:fixed;top:300px";
+	//admin_filter_block.className = "form-button";
+	//admin_filter_block.style = "position:fixed;top:300px";
 	admin_filter_block.appendChild(admin_filter_header);
 	admin_filter_block.appendChild(admin_order_filter);
 
 
 
 
-	document.getElementById('messages').appendChild(admin_filter_block);
-
+	//document.getElementById('messages').appendChild(admin_filter_block);
+	
 
 	//batch button
 
@@ -33,10 +34,16 @@ if (window.location.href.match("/sales_order/index/")) {
 	admin_batch_button.type = "button";
 	admin_batch_button.value = "Create Batch";
 	admin_batch_button.size = "16";
-	admin_batch_button.style = "margin-left:95%";
+	admin_batch_button.className = "form-button";
+	admin_batch_button.style = "margin-top:10px;"
 
 
-	document.getElementById('messages').appendChild(admin_batch_button);
+	//document.getElementById('messages').appendChild(admin_batch_button);
+
+	var elements = document.getElementsByClassName('pager');
+	elements[0].appendChild(admin_filter_block);
+	elements[0].appendChild(admin_batch_button);
+
 
 
 	jQuery('#admin_filter_block').click(function() {
